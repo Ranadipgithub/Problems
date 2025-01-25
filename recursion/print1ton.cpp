15b0036245e0e33acfce3ae559bf8dbe25665df2
Rana Dutta <duttarana189@gmail.com>
2025-01-25 11:28:14 +0530

    print1ton

---
+++ recursion/print1ton.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+void print(int n){
+    if(n==1){
+        cout<<n<<endl;
+        return;
+    }
+    print(n-1);
+    cout<<n<<endl;
+}
+
+int main(){
+    int n;
+    cout<<"Enter the number: ";
+    cin>>n;
+    print(n);
+    return 0;
+}

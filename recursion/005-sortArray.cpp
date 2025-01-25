b5f4738c5967c820c574986e72e3b04f218c2738
Rana Dutta <duttarana189@gmail.com>
2025-01-25 12:31:03 +0530

    sort array

---
+++ recursion/005-sortArray.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void insert(vector<int> &arr, int temp){
+    if(arr.size() == 0 || arr[arr.size()-1] <= temp){
+        arr.push_back(temp);
+        return;
+    }
+    int val = arr[arr.size()-1];
+    arr.pop_back();
+    insert(arr, temp);
+    arr.push_back(val);
+}
+
+void sort(vector<int> &arr){
+    if(arr.size() == 1){
+        return;
+    }
+    int temp = arr[arr.size()-1];
+    arr.pop_back();
+    sort(arr);
+    insert(arr, temp);
+}
+
+int main(){
+    int n;
+    cout << "Enter the size of the array: ";
+    cin >> n;
+    vector<int> arr(n);
+    cout << "Enter the elements of the array: ";
+    for(int i=0; i<n; i++){
+        cin >> arr[i];
+    }
+    sort(arr);
+    for(int i=0; i<n; i++){
+        cout << arr[i] << " ";
+    }
+}

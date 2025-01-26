83aa71bb2478596bcd9d711dc34ab43d5c84d1b5
Rana Dutta <duttarana189@gmail.com>
2025-01-26 10:05:39 +0530

    tower of brahma

--- recursion/010-tower_of_brahma.cpp
+++ recursion/010-tower_of_brahma.cpp
@@ -1,14 +1,17 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-void solve(int n, int s, int d, int h){
+void solve(int n, int s, int d, int h, int &count){
     if(n == 1){
+        count++;
         cout << "moving disk 1 from " << s << " to " << d << endl;
         return;
     }
-    solve(n-1, s, h, d);
+
+    solve(n-1, s, h, d, count);
     cout << "moving disk " << n << " from " << s << " to " << d << endl;
-    solve(n-1, h, d, s);
+    count++;
+    solve(n-1, h, d, s, count);
 }
 
 int main(){
@@ -16,6 +19,8 @@ int main(){
     cout << "Enter the number of disks: ";
     cin >> n;
     int s = 1, h = 2, d = 3;
-    solve(n, s, d, h);
+    int count = 0;
+    solve(n, s, d, h, count);
+    cout << "Total number of moves: " << count << endl;
     return 0;
 }

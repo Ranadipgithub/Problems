9929cc0863f2c7e58fc28bda686156dcb0c0fed8
Rana Dutta <duttarana189@gmail.com>
2025-02-20 10:15:42 +0530

    added sol for Mystic_Slimes

--- cp/Mystic_Slimes.cpp
+++ cp/Mystic_Slimes.cpp
@@ -1,45 +1,83 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
 
-vector<int> solve(const vector<int>& arr, int i, int j) {
-    if (i == j)
-        return { arr[i] };
-
-    vector<int> res;
-    for (int k = i; k < j; k++) {
-        vector<int> left = solve(arr, i, k);
-        vector<int> right = solve(arr, k + 1, j);
-        for (int x : left) {
-            for (int y : right) {
-                res.push_back(max(0, x - y));
-                res.push_back(max(0, y - x));
-            }
-        }
-    }
-    return res;
-}
-
-int main() {
-    ios::sync_with_stdio(false);
+int main(){
+    ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
 
     int t;
     cin >> t;
-    while (t--) {
+    while(t--){
         int n;
         cin >> n;
         vector<int> arr(n);
-        for (int i = 0; i < n; i++) {
+        for(int i = 0; i < n; i++){
             cin >> arr[i];
         }
-
-        vector<int> result = solve(arr, 0, n - 1);
         int ans = 0;
-        for (int x : result) {
-            ans = max(ans, x);
+        for(int i = 0;i<n;i++){
+            int left;
+            int right;
+            if(i!=1){
+                left = 0;
+            } else{
+                left = arr[0];
+            }
+            if(i!=n-1){
+                right = 0;
+            } else{
+                right = arr[n-1];
+            }
+            ans = max(ans, arr[i] - (left+right));
         }
-        cout << ans << endl;
+        cout<< ans << endl;
     }
-
     return 0;
 }
+
+
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// int solve(vector<int>& A) {
+//     int n = A.size();
+//     vector<vector<int>> dp(n, vector<int>(n, 0));
+    
+//     for (int i = 0; i < n; ++i)
+//         dp[i][i] = A[i];
+    
+//     for (int len = 2; len <= n; ++len) {
+//         for (int l = 0; l + len <= n; ++l) {
+//             int r = l + len - 1;
+//             dp[l][r] = 0;
+//             for (int k = l; k < r; ++k) {
+//                 int left = dp[l][k];
+//                 int right = dp[k+1][r];
+//                 int val1 = max(left - right, 0);
+//                 int val2 = max(right - left, 0);
+//                 dp[l][r] = max(dp[l][r], max(val1, val2));
+//             }
+//         }
+//     }
+//     return dp[0][n-1];
+// }
+
+// int main() {
+//     ios_base::sync_with_stdio(false);
+//     cin.tie(nullptr);
+    
+//     int T;
+//     cin >> T;
+//     while (T--) {
+//         int N;
+//         cin >> N;
+//         vector<int> A(N);
+//         for (int i = 0; i < N; ++i)
+//             cin >> A[i];
+//         cout << solve(A) << '\n';
+//     }
+    
+//     return 0;
+// }
+
+

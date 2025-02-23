27a88a7ac5bc5a1e750afdc0a8b9d7dadfeae91c
Rana Dutta <duttarana189@gmail.com>
2025-02-23 14:50:25 +0530

    added sol for ones and zeros problem lc

--- dp/009-ones_and_zeros_lc.cpp
+++ dp/009-ones_and_zeros_lc.cpp
@@ -24,6 +24,32 @@
 //     cout << ans;
 // }
 
+// Memoize the recursive solution
+
+// #include<bits/stdc++.h>
+// using namespace std;
+
+// vector<vector<vector<int>>> dp;
+
+// int solve(vector<string> &strs, int m, int n, int s){
+//     if(s == 0) return 0;
+//     if(dp[s][m][n] != -1) return dp[s][m][n];
+
+//     int zeros = count(strs[s-1].begin(), strs[s-1].end(), '0');
+//     int ones = count(strs[s-1].begin(), strs[s-1].end(), '1');
+
+//     if(zeros > m || ones > n) return dp[s][m][n] = solve(strs, m, n, s-1);
+//     return dp[s][m][n] = max(1 + solve(strs, m-zeros, n-ones, s-1), solve(strs, m, n, s-1));
+// }
+
+// int main(){
+//     vector<string> strs = {"10", "0001", "111001", "1", "0"};
+//     int s = strs.size();
+//     int m = 5, n = 3;
+//     dp.resize(s+1, vector<vector<int>>(m+1, vector<int>(n+1, -1)));
+//     cout << solve(strs, m, n, s) << endl;
+// }
+
 
 // DP solution
 
@@ -34,15 +60,36 @@ int main(){
     vector<string> strs = {"10", "0001", "111001", "1", "0"};
     int s = strs.size();
     int m = 5, n = 3;
-    vector<vector<int>> dp(m+1, vector<int>(n+1, 0));
-    for(int i=1; i<=s; i++){
+    vector<vector<vector<int>>> dp(s+1, vector<vector<int>>(m+1, vector<int>(n+1, 0)));
+    for(int i = 1; i<=s ;i++){
         int zeros = count(strs[i-1].begin(), strs[i-1].end(), '0');
-        int ones = count(strs[i-1].begin(), strs[i-1].end(), '1');
-        for(int j=m; j>=zeros; j--){
-            for(int k=n; k>=ones; k--){
-                dp[j][k] = max(1 + dp[j-zeros][k-ones], dp[j][k]);
+        int ones = strs[i-1].size() - zeros;
+        for(int j = 0; j<=m; j++){
+            for(int k = 0; k<=n; k++){
+                if(zeros > j || ones > k) dp[i][j][k] = dp[i-1][j][k];
+                else dp[i][j][k] = max(1 + dp[i-1][j-zeros][k-ones], dp[i-1][j][k]);
             }
         }
     }
-    cout << dp[m][n];
+    cout << dp[s][m][n];
 }
+
+// #include<bits/stdc++.h>
+// using namespace std;
+
+// int main(){
+//     vector<string> strs = {"10", "0001", "111001", "1", "0"};
+//     int s = strs.size();
+//     int m = 5, n = 3;
+//     vector<vector<int>> dp(m+1, vector<int>(n+1, 0));
+//     for(int i=1; i<=s; i++){
+//         int zeros = count(strs[i-1].begin(), strs[i-1].end(), '0');
+//         int ones = count(strs[i-1].begin(), strs[i-1].end(), '1');
+//         for(int j=m; j>=zeros; j--){
+//             for(int k=n; k>=ones; k--){
+//                 dp[j][k] = max(1 + dp[j-zeros][k-ones], dp[j][k]);
+//             }
+//         }
+//     }
+//     cout << dp[m][n];
+// }

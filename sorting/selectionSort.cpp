2d6685dc9a364cb418a00d87b3c407813ce421e1
Rana Dutta <duttarana189@gmail.com>
2025-05-05 08:46:34 +0530

    feat: implement selection sort algorithm with input handling and output display

--- sorting/selectionSort.cpp
+++ sorting/selectionSort.cpp
@@ -1,6 +1,30 @@
-#include<iostream>
+#include<bits/stdc++.h>
 using namespace std;
 
+void selectionSort(vector<int>&arr, int n){
+    for(int i = 0;i<n-1;i++){
+        int minIdx = i;
+        for(int j = i+1;j<n;j++){
+            if(arr[j] < arr[minIdx]){
+                minIdx = j;
+            }
+        }
+        if(minIdx != i){
+            swap(arr[i], arr[minIdx]);
+        }
+    }
+}
+
 int main(){
-    
+    int n;
+    cin >> n;
+    vector<int> arr(n);
+    for(int i = 0; i < n; i++){
+        cin >> arr[i];
+    }
+    selectionSort(arr, n);
+    cout << "Sorted array: \n";
+    for(int i = 0; i < n; i++){
+        cout << arr[i] << " ";
+    }
 }
